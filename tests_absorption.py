58ede46e1dc99971dba618592349b9189be0aac9
Antoine Mottet <antoine@prague>
2020-06-08 16:25:57 +0200

    essential relations, center, absorption

--- tests_absorption.py
+++ tests_absorption.py
@@ -4,11 +4,6 @@ from absorption import *
 VH = {0,1}
 EH = [ (0,1),(1,0) ]
 P1 = RelationalStructure(VH, [[EH,2]])
-P1d = RelationalStructure({'s','t'}, [[[('s','t')],2] ])
-
-VP2d = {'a','b','c'}
-EP2d = [ ('a','b'), ('b','c') ]
-P2d = RelationalStructure(VP2d, [[EP2d,2]])
 
 n = None
 for m in majority(P1):
@@ -19,3 +14,10 @@ absorbing = tuple( B for B in absorbingSubsets({0,1},n,3))
 assert(absorbing == ({0},{1},{0,1}))
 
 assert(isCenter({0,1},{0},n,3))
+
+assert(isAbsorbingClone({0,1},{0},n,3,2) == False) # {0} does not binary absorb {0,1}
+assert(isAbsorbingClone({0,1},{0},n,3,3) == True)
+for R in essentialRelations({0,1},{0},n,3,2):
+    assert(R == {(0,1),(1,0)})
+for R in essentialRelations({0,1},{0},n,3,3):
+    assert(False) # no ternary essential relation exists, since {0} ternary absorbs {0,1}
