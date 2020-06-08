3631c17523e2e091a08bcb9baefa9bb8b6775af3
Antoine Mottet <antoine@prague>
2020-06-08 11:38:04 +0200

    restructuration of the code

---
+++ solver.py
@@ -0,0 +1,74 @@
+from polymorphisms import homomorphisms
+from equations import siggers
+from csptosat import csptosat
+import logging
+
+class IntractableTemplate(Exception):
+    pass
+
+class Solver:
+    def __init__(self,rel):
+        self.template = rel
+        
+    def solve(self,X):
+        raise NotImplementedError()
+        
+
+class ACSolver(Solver):
+    def __init__(self,rel):
+        super().__init__(rel)
+    
+    def solve(self,X):
+        for h in homomorphisms(X,self.template):
+            return h
+        return None
+    
+class SATSolver(Solver):
+    def solve(self,X):
+        return csptosat(X,self.template)
+    
+# we assume the template is a core here
+class ZhukSolver(Solver):
+    def __init__(self,rel,wnu=None):
+        super().__init__(rel)
+        
+        self.poly = None
+        if wnu != None:
+            self.poly = wnu
+        else:
+            for s in siggers(self.template,idempotent=True):
+                logging.info('ZhukSolver:Found a Siggers polymorphism of the template')
+                self.poly = s
+                break
+            if self.poly == None:
+                raise IntractableTemplate
+                
+    def solve(self,X):
+        return None
+    
+    
+    
+# generates all homomorphisms from A to B that are compatible with the given initial data
+def homomorphisms(A,B, initialL=None):
+    L = consistency.arcConsistency(A,B,initialL)
+    
+    if L == None:
+        logging.info('homomorphisms:No homomorphism')
+        return None
+    
+    # Finds the next element for which the homomorphism value is not uniquely defined
+    nextElem = None
+    for a in A.domain:
+        if len(L[a])>1:
+            nextElem = a
+            break
+
+    # If no such element exists, the homomorphism is completely defined and we return it
+    if nextElem == None:
+        yield L
+        
+    else:
+        oldSet = L[a]
+        for b in oldSet:
+            L[a] = {b}
+            yield from homomorphisms(A,B,L)
---
+++ tests_solver.py
@@ -0,0 +1,34 @@
+from solver import *
+from relational_structure import *
+from polymorphisms import *
+import logging
+
+Domain = {1,2,3}
+E = [ [1,2],[2,3],[2,1],[3,2],[1,3],[3,1]]
+K3 = RelationalStructure(Domain, [[E,2]])
+
+try:
+    solver = ZhukSolver(K3)
+    assert(False) # K3 has no Taylor polymorphism, which should be detected by the solver
+except:
+    pass
+
+VP2d = {'a','b','c'}
+EP2d = [ ('a','b'), ('b','c') ]
+P2d = RelationalStructure(VP2d, [[EP2d,2]])
+VH = {0,1}
+EH = [ (0,1),(1,0) ]
+P1 = RelationalStructure(VH, [[EH,2]])
+P1d = RelationalStructure({'s','t'}, [[[('s','t')],2] ])
+
+solver = ZhukSolver(P2d)
+assert(solver.poly != None)
+assert(solver.solve(P1) == None)
+
+ac = ACSolver(P2d)
+assert(ac.solve(P1) == None)
+assert(ac.solve(P1d) != None)
+
+sat = SATSolver(P2d)
+assert(sat.solve(P1) == None)
+assert(sat.solve(P1d) != None)
